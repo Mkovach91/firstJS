64b2bac67eeefe1590a8b705b5d0a55a97f0d8fd
matt k <matt.kovach91@gmail.com>
2024-10-09 11:41:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Link HTML doc to js doc with a script link
+// write constants to test if the alert works
+// create an alert box with 3 variables in it that are pulled from the code below.
+// Once I have confirmed my pop up message works I will change all the constants to lets 
+// use simple math formulas to define the code for the pop up.
+
+
+// const vaultNum1= 5
+// const vaultNum2= 5
+// const vaultNum3= 5
+// Had to re-ordeer code - variables must be defined before the alert is wrote in JS 
+
+let vaultNum1 = 5 + 5; 
+let vaultNum2 = 50 - 15 - 3;
+let vaultNum3 = 5 * 13 - 24;
+
+alert(
+`"You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${vaultNum1}-${vaultNum2}-${vaultNum3}"`)
